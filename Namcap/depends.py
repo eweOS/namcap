eweOS/namcap

commit 4e0331f2cc69e3169e572dcaa812eebbbdfeccbf

feat: Add type annotations for AbstractRule class
The type annotations help in detecting issues when adding errors,
warnings and infos.

Signed-off-by: David Runge <dvzrv@archlinux.org>

Namcap/ruleclass.py
@@ -1,6 +1,8 @@
 # Copyright (C) 2003-2023 Namcap contributors, see AUTHORS for details.
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+from typing import Any
+
 """
 This module defines the base classes from which Namcap rules are derived
 and how they are meant to be used.
@@ -12,9 +14,9 @@ class AbstractRule(object):
     "The parent class of all rules"
 
     def __init__(self):
-        self.errors = []
-        self.warnings = []
-        self.infos = []
+        self.errors: list[tuple[str, tuple[Any, ...]]] = []
+        self.warnings: list[tuple[str, tuple[Any, ...]]] = []
+        self.infos: list[tuple[str, tuple[Any, ...]]] = []
 
 
 class PkgInfoRule(AbstractRule):

commit 7e2065b7a8b4c09c9adf5de007fac3d228cc633b

chore(mypy): Fix inconsistent arg types [arg-type]
These issues were caused by type annotations introduced in 4e0331f.
In 96f3672, the fallout was partially fixed in `licensepkg.py` but not
the rest of the codebase.

Fix the issue for all occurrences I could spot.
I mainly inspected all `arg-type` violations, but I also fixed other
occurrences that mypy can’t detect yet whenever I bumped into one.

CC: David Runge <dvzrv@archlinux.org>

Namcap/depends.py
@@ -6,6 +6,8 @@
 import Namcap.tags
 from Namcap import package
 
+from .types import Diagnostic
+
 
 def single_covered(depend):
     "Returns full coverage tree of one package, with loops broken"
@@ -48,7 +50,9 @@ def getprovides(depends):
 
 
 def analyze_depends(pkginfo):
-    errors, warnings, infos = [], [], []
+    errors: list[Diagnostic] = []
+    warnings: list[Diagnostic] = []
+    infos: list[Diagnostic] = []
 
     # compute needed dependencies
     dependlist = set(pkginfo.detected_deps.keys())
@@ -74,11 +78,11 @@ def analyze_depends(pkginfo):
 
     # Common deps
     [
-        errors.append(("dependency-duplicated-optdepend %s", duplicated_optdepend))
+        errors.append(("dependency-duplicated-optdepend %s", (duplicated_optdepend,)))
         for duplicated_optdepend in explicitdepend & optdepend
     ]
     [
-        infos.append(("dependency-satisfied-optdepend %s", satisfied_optdepend))
+        infos.append(("dependency-satisfied-optdepend %s", (satisfied_optdepend,)))
         for satisfied_optdepend in implicitdepend & optdepend
     ]
 
@@ -119,7 +123,7 @@ def analyze_depends(pkginfo):
         #   it is not in the depends as we see them
         #   it does not pull some needed dependency which provides it
         if i not in dependlist and i not in allprovides:
-            warnings.append(("dependency-not-needed %s", i))
-    infos.append(("depends-by-namcap-sight depends=(%s)", " ".join(dependlist)))
+            warnings.append(("dependency-not-needed %s", (i,)))
+    infos.append(("depends-by-namcap-sight depends=(%s)", (" ".join(dependlist),)))
 
     return errors, warnings, infos

Namcap/ruleclass.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2003-2023 Namcap contributors, see AUTHORS for details.
 # SPDX-License-Identifier: GPL-2.0-or-later
 
-from typing import Any
+from .types import Diagnostic
 
 """
 This module defines the base classes from which Namcap rules are derived
@@ -16,9 +16,9 @@ class AbstractRule(object):
     enable: bool = True
 
     def __init__(self):
-        self.errors: list[tuple[str, tuple[Any, ...]]] = []
-        self.warnings: list[tuple[str, tuple[Any, ...]]] = []
-        self.infos: list[tuple[str, tuple[Any, ...]]] = []
+        self.errors: list[Diagnostic] = []
+        self.warnings: list[Diagnostic] = []
+        self.infos: list[Diagnostic] = []
 
 
 class PkgInfoRule(AbstractRule):

Namcap/rules/arrays.py
@@ -41,4 +41,4 @@ class package(PkgbuildRule):
             for j in arrayvars:
                 if m and m.group(1) == j:
                     if not m.group(2).startswith("("):
-                        self.warnings.append(("variable-not-array %s", j))
+                        self.warnings.append(("variable-not-array %s", (j,)))

Namcap/rules/carch.py
@@ -28,4 +28,4 @@ class package(PkgbuildRule):
                 continue
             if "${CARCH}" in line:
                 continue
-            self.warnings.append(("specific-host-type-used %s", match.group(1)))
+            self.warnings.append(("specific-host-type-used %s", (match.group(1),)))

Namcap/rules/elffiles.py
@@ -53,9 +53,9 @@ class ELFPaths(TarballRule):
                     invalid_elffiles.append(entry.name)
 
         que_elfdirs = [d for d in questionable_dirs if any(f.startswith(d) for f in questionable_elffiles)]
-        self.errors = [("elffile-not-in-allowed-dirs %s", i) for i in invalid_elffiles]
+        self.errors = [("elffile-not-in-allowed-dirs %s", (i,)) for i in invalid_elffiles]
         self.errors.extend(("elffile-in-questionable-dirs %s", (i,)) for i in que_elfdirs)
-        self.infos = [("elffile-not-in-allowed-dirs %s", i) for i in questionable_elffiles]
+        self.infos = [("elffile-not-in-allowed-dirs %s", (i,)) for i in questionable_elffiles]
 
 
 class ELFTextRelocationRule(TarballRule):
@@ -78,7 +78,7 @@ class ELFTextRelocationRule(TarballRule):
                         files_with_textrel.append(entry_name)
 
         if files_with_textrel:
-            self.warnings = [("elffile-with-textrel %s", i) for i in files_with_textrel]
+            self.warnings = [("elffile-with-textrel %s", (i,)) for i in files_with_textrel]
 
 
 class ELFExecStackRule(TarballRule):
@@ -106,7 +106,7 @@ class ELFExecStackRule(TarballRule):
                     exec_stacks.append(entry_name)
 
         if exec_stacks:
-            self.warnings = [("elffile-with-execstack %s", i) for i in exec_stacks]
+            self.warnings = [("elffile-with-execstack %s", (i,)) for i in exec_stacks]
 
 
 class ELFGnuRelroRule(TarballRule):
@@ -146,7 +146,7 @@ class ELFGnuRelroRule(TarballRule):
             missing_relro.append(entry_name)
 
         if missing_relro:
-            self.warnings = [("elffile-without-relro %s", i) for i in missing_relro]
+            self.warnings = [("elffile-without-relro %s", (i,)) for i in missing_relro]
 
 
 class ELFUnstrippedRule(TarballRule):
@@ -176,7 +176,7 @@ class ELFUnstrippedRule(TarballRule):
                 if section.name == ".symtab":
                     unstripped_binaries.append(entry_name)
         if unstripped_binaries:
-            self.warnings = [("elffile-unstripped %s", i) for i in unstripped_binaries]
+            self.warnings = [("elffile-unstripped %s", (i,)) for i in unstripped_binaries]
 
 
 class NoPIERule(TarballRule):
@@ -205,7 +205,7 @@ class NoPIERule(TarballRule):
                 nopie_binaries.append(entry_name)
 
         if nopie_binaries:
-            self.warnings = [("elffile-nopie %s", i) for i in nopie_binaries]
+            self.warnings = [("elffile-nopie %s", (i,)) for i in nopie_binaries]
 
 
 def _note_props(elffile, note_type, prop_type):
@@ -251,4 +251,4 @@ class ELFSHSTKRule(TarballRule):
             else:
                 noshstk_binaries.append(entry_name)
         if noshstk_binaries:
-            self.warnings = [("elffile-noshstk %s", i) for i in noshstk_binaries]
+            self.warnings = [("elffile-noshstk %s", (i,)) for i in noshstk_binaries]

Namcap/rules/externalhooks.py
@@ -32,4 +32,4 @@ class ExternalHooksRule(TarballRule):
         f.close()
         for command in self.hooked:
             if command in text:
-                self.warnings.append(("external-hooks %s", command))
+                self.warnings.append(("external-hooks %s", (command,)))

Namcap/rules/hookdepends.py
@@ -27,4 +27,4 @@ class HookDependsRule(TarballRule):
                 continue
             pattern = re.compile(subrule["path"])
             if any(pattern.search(n) for n in names):
-                self.warnings.append(("external-hooks-unneeded %s", dep))
+                self.warnings.append(("external-hooks-unneeded %s", (dep,)))

Namcap/rules/lotsofdocs.py
@@ -24,4 +24,4 @@ class package(TarballRule):
         if size > 0:
             ratio = docsize / float(size)
             if ratio > 0.50:
-                self.warnings.append(("lots-of-docs %f", ratio * 100))
+                self.warnings.append(("lots-of-docs %f", (ratio * 100,)))

Namcap/rules/makedepends.py
@@ -23,7 +23,7 @@ class RedundantMakedepends(PkgbuildRule):
         redundant_makedeps.extend(set(pkginfo["makedepends"]) & set(pkginfo["depends"]))
 
         for i in redundant_makedeps:
-            self.warnings.append(("redundant-makedep %s", i))
+            self.warnings.append(("redundant-makedep %s", (i,)))
 
 
 class VCSMakedepends(PkgbuildRule):
@@ -66,4 +66,4 @@ class VCSMakedepends(PkgbuildRule):
                 missing.append(d)
 
         for i in missing:
-            self.warnings.append(("missing-vcs-makedeps %s", i))
+            self.warnings.append(("missing-vcs-makedeps %s", (i,)))

Namcap/rules/shebangdepends.py
@@ -92,4 +92,4 @@ class ShebangDependsRule(TarballRule):
             p = Namcap.package.load_testing_package(i)
             q = Namcap.package.load_from_db(i)
             if p is not None and q is not None and p["version"] == q["version"]:
-                self.warnings.append(("dependency-is-testing-release %s", i))
+                self.warnings.append(("dependency-is-testing-release %s", (i,)))

Namcap/rules/sodepends.py
@@ -215,14 +215,14 @@ class SharedLibsRule(TarballRule):
 
         for i in pkginfo["depends"]:
             if ".so" in i and i not in libdependlist:
-                self.warnings.append(("libdepends-not-needed %s", i))
+                self.warnings.append(("libdepends-not-needed %s", (i,)))
             if i.endswith(".so"):
-                self.errors.append(("libdepends-without-version %s", i))
+                self.errors.append(("libdepends-without-version %s", (i,)))
 
         self.infos.append(
             (
                 "libdepends-by-namcap-sight depends=(%s)",
-                " ".join(sorted(set(libdependlist) | set(missing_provides.values()))),
+                (" ".join(sorted(set(libdependlist) | set(missing_provides.values()))),),
             )
         )
 
@@ -235,15 +235,15 @@ class SharedLibsRule(TarballRule):
 
         for i in pkginfo["provides"]:
             if ".so" in i and i not in libprovides:
-                self.warnings.append(("libprovides-missing %s", i))
+                self.warnings.append(("libprovides-missing %s", (i,)))
             if i.endswith(".so"):
-                self.errors.append(("libprovides-without-version %s", i))
+                self.errors.append(("libprovides-without-version %s", (i,)))
 
-        self.infos.append(("libprovides-by-namcap-sight provides=(%s)", " ".join(libprovides)))
+        self.infos.append(("libprovides-by-namcap-sight provides=(%s)", (" ".join(libprovides),)))
 
         # Check for packages in testing
         for i in dependlist.keys():
             p = Namcap.package.load_testing_package(i)
             q = Namcap.package.load_from_db(i)
             if p is not None and q is not None and p["version"] == q["version"]:
-                self.warnings.append(("dependency-is-testing-release %s", i))
+                self.warnings.append(("dependency-is-testing-release %s", (i,)))

Namcap/rules/splitpkgbuild.py
@@ -23,7 +23,7 @@ class PackageFunctionsRule(PkgbuildRule):
         # All subpackages should have a valid package function
         for subpkg in pkginfo.subpackages:
             if subpkg["pkgfunction"] != "function":
-                self.errors.append(("missing-pkgfunction %s", subpkg["name"]))
+                self.errors.append(("missing-pkgfunction %s", (subpkg["name"],)))
 
 
 class SplitPkgMakedepsRule(PkgbuildRule):
@@ -59,4 +59,4 @@ class SplitPkgMakedepsRule(PkgbuildRule):
 
         if not local_deps.issubset(global_deps):
             missing = list(local_deps - global_deps)
-            self.errors.append(("missing-makedeps %s", str(sorted(missing))))
+            self.errors.append(("missing-makedeps %s", (str(sorted(missing)),)))

Namcap/types.py
@@ -1,0 +1,8 @@
+# Copyright (C) 2003-2023 Namcap contributors, see AUTHORS for details.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+from typing import Any, TypeAlias
+
+FormatArgs: TypeAlias = tuple[Any, ...]
+
+Diagnostic: TypeAlias = tuple[str, FormatArgs]

pyproject.toml
@@ -18,7 +18,6 @@ line-length = 120
 [tool.mypy]
 check_untyped_defs = true
 disable_error_code = """
-    arg-type,
     assignment,
     attr-defined,
     func-returns-value,

Namcap/tests/package/test_elffiles.py
@@ -32,7 +32,7 @@ package() {
             f.write(self.pkgbuild)
         self.run_makepkg()
         pkg, r = self.run_rule_on_tarball(os.path.join(self.tmpdir, pkgfile), Namcap.rules.elffiles.ELFPaths)
-        self.assertEqual(r.errors, [("elffile-not-in-allowed-dirs %s", "usr/share/something/binary")])
+        self.assertEqual(r.errors, [("elffile-not-in-allowed-dirs %s", ("usr/share/something/binary",))])
         self.assertEqual(r.warnings, [])
         self.assertEqual(r.infos, [])
 
@@ -66,7 +66,7 @@ package() {
         self.run_makepkg()
         pkg, r = self.run_rule_on_tarball(os.path.join(self.tmpdir, pkgfile), Namcap.rules.elffiles.ELFExecStackRule)
         self.assertEqual(r.errors, [])
-        self.assertEqual(r.warnings, [("elffile-with-execstack %s", "usr/bin/withexecstack")])
+        self.assertEqual(r.warnings, [("elffile-with-execstack %s", ("usr/bin/withexecstack",))])
         self.assertEqual(r.infos, [])
 
 
@@ -99,7 +99,7 @@ package() {
         self.run_makepkg()
         pkg, r = self.run_rule_on_tarball(os.path.join(self.tmpdir, pkgfile), Namcap.rules.elffiles.NoPIERule)
         self.assertEqual(r.errors, [])
-        self.assertEqual(r.warnings, [("elffile-nopie %s", "usr/bin/nopie")])
+        self.assertEqual(r.warnings, [("elffile-nopie %s", ("usr/bin/nopie",))])
         self.assertEqual(r.infos, [])
 
 
@@ -143,9 +143,9 @@ package() {
         self.assertEqual(
             r.warnings,
             [
-                ("elffile-noshstk %s", "usr/bin/fcf-protection-branch"),
-                ("elffile-noshstk %s", "usr/bin/fcf-protection-none"),
-                ("elffile-noshstk %s", "usr/bin/no-.note.gnu.property"),
+                ("elffile-noshstk %s", ("usr/bin/fcf-protection-branch",)),
+                ("elffile-noshstk %s", ("usr/bin/fcf-protection-none",)),
+                ("elffile-noshstk %s", ("usr/bin/no-.note.gnu.property",)),
             ],
         )
         self.assertEqual(r.infos, [])

Namcap/tests/package/test_externalhooks.py
@@ -52,7 +52,7 @@ post_install() {
         self.assertEqual(r.errors, [])
         self.assertEqual(
             r.warnings,
-            [("external-hooks %s", "update-desktop-database"), ("external-hooks %s", "gtk-update-icon-cache")],
+            [("external-hooks %s", ("update-desktop-database",)), ("external-hooks %s", ("gtk-update-icon-cache",))],
         )
         self.assertEqual(r.infos, [])
 

Namcap/tests/package/test_hicoloricons.py
@@ -47,7 +47,7 @@ package() {
             ),
         )
         self.assertEqual(r.warnings + w, [])
-        self.assertEqual(r.infos + i, [("depends-by-namcap-sight depends=(%s)", "hicolor-icon-theme")])
+        self.assertEqual(r.infos + i, [("depends-by-namcap-sight depends=(%s)", ("hicolor-icon-theme",))])
 
     valid_pkgbuild = """
 pkgname=__namcap_test_hicoloricons
@@ -82,6 +82,6 @@ package() {
             r.infos + i,
             [
                 ("dependency-detected-satisfied %s (%s)", ("hicolor-icon-theme", "needed for hicolor theme hierarchy")),
-                ("depends-by-namcap-sight depends=(%s)", "hicolor-icon-theme"),
+                ("depends-by-namcap-sight depends=(%s)", ("hicolor-icon-theme",)),
             ],
         )

Namcap/tests/package/test_lotsofdocs.py
@@ -37,5 +37,5 @@ package() {
         self.assertEqual(r.errors, [])
         tag, value = r.warnings[0]
         self.assertEqual(tag, "lots-of-docs %f")
-        self.assertGreater(value, 50)
+        self.assertGreater(value, (50,))
         self.assertEqual(r.infos, [])

Namcap/tests/pkgbuild/test_arrays.py
@@ -43,5 +43,5 @@ package() {
         "Variables in PKGBUILD are not an array"
         r = self.run_on_pkg(self.pkgbuild1)
         self.assertEqual(r.errors, [])
-        self.assertEqual(set(r.warnings), set(("variable-not-array %s", i) for i in ["depends", "license"]))
+        self.assertEqual(set(r.warnings), set(("variable-not-array %s", (i,)) for i in ["depends", "license"]))
         self.assertEqual(r.infos, [])

Namcap/tests/pkgbuild/test_carch.py
@@ -59,5 +59,5 @@ package() {
     def test_example2(self):
         r = self.run_on_pkg(self.pkgbuild2)
         self.assertEqual(r.errors, [])
-        self.assertEqual(r.warnings, [("specific-host-type-used %s", "i686")])
+        self.assertEqual(r.warnings, [("specific-host-type-used %s", ("i686",))])
         self.assertEqual(r.infos, [])

Namcap/tests/pkgbuild/test_makedepends.py
@@ -40,7 +40,7 @@ package() {
         # Example 1
         r = self.run_on_pkg(self.pkgbuild1)
         self.assertEqual(r.errors, [])
-        self.assertEqual(set(r.warnings), set(("redundant-makedep %s", i) for i in ["lib1", "lib2"]))
+        self.assertEqual(set(r.warnings), set(("redundant-makedep %s", (i,)) for i in ["lib1", "lib2"]))
         self.assertEqual(r.infos, [])
 
 
@@ -83,7 +83,7 @@ package() {
         makedeps = ["breezy", "git", "mercurial", "subversion"]
         r = self.run_on_pkg(self.pkgbuild1)
         self.assertEqual(r.errors, [])
-        self.assertEqual(set(r.warnings), set(("missing-vcs-makedeps %s", i) for i in makedeps))
+        self.assertEqual(set(r.warnings), set(("missing-vcs-makedeps %s", (i,)) for i in makedeps))
         self.assertEqual(r.infos, [])
 
     def test_example2(self):

Namcap/tests/pkgbuild/test_splitpkgbuild.py
@@ -44,7 +44,7 @@ package_prog1() {
     def test_example1(self):
         "Example 1 : missing package_* function"
         r = self.run_on_pkg(self.pkgbuild1)
-        self.assertEqual(r.errors, [("missing-pkgfunction %s", "prog2")])
+        self.assertEqual(r.errors, [("missing-pkgfunction %s", ("prog2",))])
         self.assertEqual(r.warnings, [])
         self.assertEqual(r.infos, [])
 
@@ -96,6 +96,6 @@ package_prog2() {
     def test_example1(self):
         "Example 1: missing makedepend"
         r = self.run_on_pkg(self.pkgbuild1)
-        self.assertEqual(r.errors, [("missing-makedeps %s", str(["lib1", "lib2"]))])
+        self.assertEqual(r.errors, [("missing-makedeps %s", (str(["lib1", "lib2"]),))])
         self.assertEqual(r.warnings, [])
         self.assertEqual(r.infos, [])

Namcap/tests/test_depends.py
@@ -16,15 +16,15 @@ class DependsTests(unittest.TestCase):
         expected_e = [("dependency-detected-not-included %s (%s)", ("pkg1", ""))]
         self.assertEqual(e, expected_e)
         self.assertEqual(w, [])
-        self.assertEqual(i, [("depends-by-namcap-sight depends=(%s)", "pkg1")])
+        self.assertEqual(i, [("depends-by-namcap-sight depends=(%s)", ("pkg1",))])
 
     def test_unneeded(self):
         self.pkginfo["depends"] = {"pkg1": []}
         e, w, i = Namcap.depends.analyze_depends(self.pkginfo)
-        expected_w = [("dependency-not-needed %s", "pkg1")]
+        expected_w = [("dependency-not-needed %s", ("pkg1",))]
         self.assertEqual(e, [])
         self.assertEqual(w, expected_w)
-        self.assertEqual(i, [("depends-by-namcap-sight depends=(%s)", "")])
+        self.assertEqual(i, [("depends-by-namcap-sight depends=(%s)", ("",))])
 
     def test_satisfied(self):
         # false negative test