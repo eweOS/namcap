3f71740490d90ba256195381cbf72f11d59446b0
Claudia Pellegrino <aur@cpellegrino.de>
2024-05-26 01:42:10 +0200

    chore(test): add rudimentary test for all_rules

---
+++ Namcap/tests/test_rules.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2024 Namcap contributors, see AUTHORS for details.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import unittest
+import Namcap.rules
+
+
+class RulesTests(unittest.TestCase):
+    def test_all_rules_dict(self):
+        self.assertGreaterEqual(len(Namcap.rules.all_rules), 61)
